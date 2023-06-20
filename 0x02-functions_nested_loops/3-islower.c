4819b55b9858bbec474a519077318fe0569d794c
15london <lilpromzy15@gmail.com>
2023-06-20 16:20:48 -0700

    3-islower git commit -m3-islower
    cd 0x02-functions_nested_loopsroot@0de85e8108dd:/alx-low_level_programming# git commit -m3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * _isalpha - function to check if c is a letter, lowercase or uppercase
- * @c: is the int that will use for the argument of the function
- * Return: 0
- */
-int _isalpha(int c)
+  * _islower - Checks for lowercase character
+  * @c: The character to be checked
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+  */
+int _islower(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 97 && c <= 122)
 	{
-	return (1);
+		return (1);
 	}
-	else
+
 	return (0);
 }
